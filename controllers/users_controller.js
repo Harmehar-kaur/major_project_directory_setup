c3c0bb8d50eb0413a99da829cd375242b5760056
Harmehar-kaur <kaurharmehar9717@gmail.com>
2023-10-09 21:33:24 +0530

    Sign up and sign in created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,49 @@
+const User =require('../models/user');
+
 module.exports.profile = function(req,res){
-    res.end('<h1>User Profile</h1>');
+    res.render('user_profile',{
+        title: 'User Profile'
+    })
+}
+
+//render the sign up page 
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up"
+    })
+}
+
+//render the Sign in page 
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codeial | Sign In"
+    })
+}
+
+module.exports.create=function(req,res){
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log('Error in finding the user in signing up');
+            return;
+        }
+
+        if(!user){
+            User.create(req.body, function(err,user){
+                if (err){
+                    console.log('Error in creating user while signing up');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
+}
+
+module.exports.createSession = function(req,res){
+    
 }
