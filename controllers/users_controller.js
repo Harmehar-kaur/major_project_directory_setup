cc4172bd45a09ca75cf2837ae974a2227c2cc300
Harmehar-kaur <kaurharmehar9717@gmail.com>
2023-10-16 12:56:47 +0530

    user_controller updated

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,20 @@
 const User =require('../models/user');
 
 module.exports.profile = function(req,res){
-    res.render('user_profile',{
-        title: 'User Profile'
-    })
+
+    if (req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(user){
+                res.render('user_profile',{
+                    title: 'User Profile',
+                    user
+                })
+            }
+            return res.redirect('/users/sign-in');
+        })
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 
 //render the sign up page 
@@ -45,5 +56,22 @@ module.exports.create=function(req,res){
 }
 
 module.exports.createSession = function(req,res){
-    
+    //find the user 
+    user.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log('Error in finding the user in signing up');
+            return;
+        }
+        //user found 
+        if(user){
+            if(user.password!= req.body.password){
+                return res.redirect('back');
+            }
+            res.cookie('user_is',user.id);
+            return res.redirect('/users/profile');
+        }else{
+            //user not found 
+            return res.redirect('back');
+        }
+    })
 }
