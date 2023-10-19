d4922e26575e9b76e9b5c01d2a2457574b192be5
Harmehar-kaur <kaurharmehar9717@gmail.com>
2023-10-19 12:43:13 +0530

    post schema and home.ejs has post form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,7 +8,7 @@ module.exports.profile = function(req,res){
 
 //render the sign up page 
 module.exports.signUp = function(req,res){
-    if(req.isAuthencated()){
+    if(req.isAuthenticated()){
         return res.redirect('users/profile');
     }
 
@@ -19,7 +19,7 @@ module.exports.signUp = function(req,res){
 
 //render the Sign in page 
 module.exports.signIn = function(req,res){
-    if(req.isAuthencated()){
+    if(req.isAuthenticated()){
        return res.redirect('users/profile');
     }
 
@@ -28,29 +28,26 @@ module.exports.signIn = function(req,res){
     })
 }
 
-module.exports.create=function(req,res){
-    if (req.body.password != req.body.confirm_password){
+module.exports.create = async function (req, res) {
+    try {
+      if (req.body.password !== req.body.confirm_password) {
         return res.redirect('back');
+      }
+  
+      const user = await User.findOne({ email: req.body.email });
+  
+      if (!user) {
+        const newUser = await User.create(req.body);
+        return res.redirect('/users/sign-in');
+      } else {
+        return res.redirect('back');
+      }
+    } catch (err) {
+      console.log('Error in signing up:', err);
+      return res.redirect('back');
     }
-    User.findOne({email:req.body.email},function(err,user){
-        if(err){
-            console.log('Error in finding the user in signing up');
-            return;
-        }
-
-        if(!user){
-            User.create(req.body, function(err,user){
-                if (err){
-                    console.log('Error in creating user while signing up');
-                    return;
-                }
-                return res.redirect('/users/sign-in');
-            })
-        }else{
-            return res.redirect('back');
-        }
-    })
-}
+  }
+  
 
 module.exports.createSession = function(req,res){
     return(res.redirect('/'));    
