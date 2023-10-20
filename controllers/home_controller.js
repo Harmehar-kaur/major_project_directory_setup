bc8440bc29e5111de6cd0dfa21dc623cc4f2a76c
Harmehar-kaur <kaurharmehar9717@gmail.com>
2023-10-20 23:25:35 +0530

    check authentication on creation of post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,20 @@
 const Post = require('../models/post');
 
-module.exports.home = function(req,res){
+module.exports.home = function(req, res) {
+    // Post.find({}, function(err, posts) {
+    //     return res.render('home', {
+    //         title: 'Home',
+    //         posts: posts
+    //     });
+    // });
 
-    Post.find{ }, function(err,posts){
-        return res.render('home',{
-            title: 'Home', 
+    // populating the user 
+    Post.find({}).populate('user').exec(function (err,posts){
+        return res.render('home', {
+            title: 'Home',
             posts: posts
         });
-    }
-} 
+    })
+}
+
 
