f354d4c846c95b873e8883b9a50f370cbcbff794
Harmehar-kaur <kaurharmehar9717@gmail.com>
2023-10-23 15:19:28 +0530

    the files updated for not getting the session working

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,54 +1,80 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = async function (req, res) {
-    try {
-        const post = await Post.findById(req.body.post).exec();
-        
-        if (post) {
-            const comment = await Comment.create({
+module.exports.create = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.body.post);
+
+        if (post){
+            let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
             });
 
-            post.comment.push(comment);
-            await post.save();
+            post.comments.push(comment);
+            post.save();
+
+            if (req.xhr){
+                // Similar for comments to fetch the user's id!
+                comment = await comment.populate('user', 'name').execPopulate();
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
+
+            req.flash('success', 'Comment published!');
 
             res.redirect('/');
         }
-    } catch (err) {
-        console.log('Error in creating the comment', err);
-        // Handle the error here
-        res.status(500).json({ error: 'An error occurred' });
+    }catch(err){
+        req.flash('error', err);
+        return;
     }
+    
 }
 
 
-module.exports.destroy = async function(req,res){
+module.exports.destroy = async function(req, res){
 
     try{
-     let comment = await Comment.findById(req.params.id).exec(); 
-    
-     if(comment.user == req.user.id){
-        const postId = comment.post;
-        await comment.remove();
-        
-        const post = Post.findByIdAndUpdate(postId, {$pull : {comments: req.params.id}}).exec()
+        let comment = await Comment.findById(req.params.id);
 
-        if (post){
+        if (comment.user == req.user.id){
+
+            let postId = comment.post;
+
+            comment.remove();
+
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
+            req.flash('success', 'Comment deleted!');
+
+            return res.redirect('back');
+        }else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
-        else {
-            // Handle the case where the post was not found
-            res.status(404).send('Post not found');
-        }
-     } else {
-        return res.redirect('back');
-     }
-    } catch (err) {
-    console.error('Error in deleting the comment', err);
-    // Handle the error here
-    res.status(500).json({ error: 'An error occurred' });   
+    }catch(err){
+        req.flash('error', err);
+        return;
     }
+    
 }
--- index.js
+++ index.js
@@ -3,79 +3,79 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
-const db =require('./config/mongoose');
-
-//used for session cookie and authentication passport
+const db = require('./config/mongoose');
+// used for session cookie
 const session = require('express-session');
 const passport = require('passport');
-const passportLocal = require('./config/passport-local-stratergy');
+const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 
-//flash 
-const flash = require('connect-flash'); 
 
-const customMware = require('./config/middleware'); 
 app.use(sassMiddleware({
-    src:'./assets/scss',
+    src: './assets/scss',
     dest: './assets/css',
-    debug: true , 
+    debug: true,
     outputStyle: 'extended',
-    prefix:'/css'
+    prefix: '/css'
 }));
 app.use(express.urlencoded());
+
 app.use(cookieParser());
+
 app.use(express.static('./assets'));
+
 app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
-//extract style and scripts
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true);
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-
-app.set('view engine','ejs');
-app.set('views','./views');
-
-
-
-
+// mongo store is used to store the session cookie in the db
 app.use(
-    session({
-      name: 'codeial',
-      secret: 'blahsomething',
-      saveUninitialized: false,
-      resave: false,
-      cookie: {
-        maxAge: 1000 * 60 * 100,
-      },
-      store: new MongoStore({
-        mongoUrl: 'mongodb://127.0.0.1:27017/codeial_development', // Replace with your MongoDB URL
-        mongooseConnection: db,
-        autoRemove: 'disabled'
-      }, function (err) {
-        console.log(err || 'connect-mongodb setup ok');
-      })
+  session({
+    name: 'codeial',
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: new MongoStore({
+      mongoUrl: 'mongodb://127.0.0.1:27017/codeial_development', // Replace with your MongoDB URL
+      mongooseConnection: db,
+      autoRemove: 'disabled'
+    }, function (err) {
+      console.log(err || 'connect-mongodb setup ok');
     })
-  );
-  
+  })
+);
 
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-//authenticating the user 
 app.use(passport.setAuthenticatedUser);
 
-// flash is used within session cookies hence declared after that
-app.use(flash()); 
-app.use(customMware.setFlash); 
+app.use(flash());
+app.use(customMware.setFlash);
 
-// use express router 
-app.use('/', require('./routes')); 
+// use express router
+app.use('/', require('./routes'));
 
-app.listen(port,function(err){
-    if(err){
-        console.log(`Error in running the server: ${err}`); 
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
     }
+
     console.log(`Server is running on port: ${port}`);
 });
+
+
+
