685eaf2c04568e66324e2b6ecd1ddecb76553246
Harmehar-kaur <kaurharmehar9717@gmail.com>
2023-10-23 19:51:53 +0530

    the ajax is added for delete task

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,31 +1,32 @@
 const passport = require('passport');
 
-const LocalStratergy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 const User =require('../models/user'); 
 //authentication
-passport.use(new LocalStratergy({
-    usernameField:'email',
-    passReqToCallback: true 
-    }, 
-    function(email,password,done){
-        //find a user and establish the identity
-        User.findOne({email:email},function(err, user){
-            if(err){
-                req.flash('error',err);
-                return done(err);
-            }
-            //user not found or password mismatch
-            if(!user || user.password != password){
-                req.flash('error','Invalid Username/Password');
-                return done(null,false);
-            }
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'email',
+    passReqToCallback: true
+  },
+  async function (req, email, password, done) {
+    try {
+      // Find a user and establish the identity
+      const user = await User.findOne({ email: email });
 
-            return done(null,user);
-        });
+      if (!user || user.password !== password) {
+        req.flash('error', 'Invalid Username/Password');
+        return done(null, false);
+      }
+
+      return done(null, user);
+    } catch (err) {
+      req.flash('error', err);
+      return done(err);
     }
-
+  }
 ));
+
 
 
 //serializing the user to decide which key is to be kept in the cookies 
@@ -35,16 +36,22 @@ passport.serializeUser(function(user,done){
 
 
 //deserializing the user from the key in the cookies 
-passport.deserializeUser(function(id,done){
-    User.findById(id ,function(err,user){
-        if(err){
-            console.log('Error in finding user --> Passport');
-            return done(err);
-        }
-
-        return done(null,user);
-    });
-});
+passport.deserializeUser(async function (id, done) {
+    try {
+      const user = await User.findById(id);
+      
+      if (!user) {
+        console.log('User not found in the database');
+        return done(null, false);
+      }
+  
+      return done(null, user);
+    } catch (err) {
+      console.log('Error in finding user --> Passport');
+      return done(err);
+    }
+  });
+  
 
 passport.checkAuthentication =function(req,res,next){
     if(req.isAuthenticated()){
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,36 +1,34 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = async function(req, res){
-    try{
-        let post = await Post.create({
-            content: req.body.content,
-            user: req.user._id
-        });
-        
-        if (req.xhr){
-            // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
-            post = await post.populate('user', 'name').execPopulate();
-
-            return res.status(200).json({
-                data: {
-                    post: post
-                },
-                message: "Post created!"
-            });
-        }
-
-        req.flash('success', 'Post published!');
-        return res.redirect('back');
-
-    }catch(err){
-        req.flash('error', err);
-        // added this to view the error on console as well
-        console.log(err);
-        return res.redirect('back');
-    }
+module.exports.create = async function (req, res) {
+    try {
+      const post = await Post.create({
+        content: req.body.content,
+        user: req.user._id
+      });
   
-}
+      if (req.xhr) {
+        // If you want to populate the 'user' field, you can use the 'populate' method without 'execPopulate'.
+        await post.populate('user', 'name');
+  
+        return res.status(200).json({
+          data: {
+            post: post
+          },
+          message: "Post created!"
+        });
+      }
+  
+      req.flash('success', 'Post published!');
+      return res.redirect('back');
+    } catch (err) {
+      req.flash('error', err.message); // Use err.message to get the error message.
+      console.log(err);
+      return res.redirect('back');
+    }
+  }
+  
 
 
 module.exports.destroy = async function(req, res){
