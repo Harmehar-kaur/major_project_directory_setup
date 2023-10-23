dd4179c143f38faa942c9580ccd74266f639df60
Harmehar-kaur <kaurharmehar9717@gmail.com>
2023-10-23 14:01:32 +0530

    ajax started

--- routes/comments.js
+++ routes/comments.js
@@ -2,7 +2,7 @@ const express= require('express');
 const router = express.Router();
 const passport = require('passport');
 
-const commentssController = require('../controllers/comments_controller'); 
+const commentsController = require('../controllers/comments_controller'); 
 
 router.post('/create', passport.checkAuthentication ,commentsController.create);
 
